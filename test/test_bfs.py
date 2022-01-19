ec35f64c0bf444c0a90ef334d53255e74249538f
Haleigh Miller <haleighmiller@Haleighs-MacBook-Pro.local>
2022-01-19 14:15:58 -0800

    fix bug

--- test/test_bfs.py
+++ test/test_bfs.py
@@ -19,7 +19,7 @@ def test_bfs_traversal():
     Check if function is traversing breadth-first properly
     Starting with A, it should go layer by layer: A, B, C, D, E, F, G
     """
-    g=graph.Graph('dummy.adjlist')
+    g=graph.Graph('test/dummy.adjlist')
     trav=g.bfs('A')
     assert trav == ['A','B','C','D','E','F','G']
 
@@ -28,7 +28,7 @@ def test_bfs_path_exists():
     Check if function finds the shortest path between start and end node
     Starting with A and going to D, the shortest path is A->B->D
     """
-    g=graph.Graph('dummy.adjlist')
+    g=graph.Graph('test/dummy.adjlist')
     shortest_path=g.bfs('A','D')
     assert shortest_path == ['A','B','D']
 
@@ -37,7 +37,7 @@ def test_bfs_path_DNE():
     Check if function finds returns None if no path exists between two nodes
     There is no path between D and G, so function should return None
     """
-    g=graph.Graph('dummy.adjlist')
+    g=graph.Graph('test/dummy.adjlist')
     non_path=g.bfs('D','G')
     assert non_path == None
 
